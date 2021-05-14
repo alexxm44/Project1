43155daef86b97e971922473a99357a6bae582f4
Alex M <Alex@Alexs-Air.attlocal.net>
2021-05-13 19:12:46 -0700

    finish structured english

---
+++ project_1.py
@@ -0,0 +1,28 @@
+"""
+*Structured English*
+Create a program that reads a list of the phone numbers below and formats
+each number with parentheses and a hyphen.
+Basic 10-digit phone number, add ().
+If it is a 11-digit phone number, add +1 ().
+Example:
+Phone numbers input:
+10-digit:
+1198628896
+5556786777
+5597872056
+..
+...
+11-digit:
+11198628896
+..
+...
+Phone number output goal:
+10-digit:
+(119)862-2886
+(555)678-6777
+(559)787-2055
+11-digit:
++1(119)862-8896
+..
+...
+"""
